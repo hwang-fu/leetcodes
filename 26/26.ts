6e2525ae716aaa01beec55dbfa8a8e2af13458c2
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-09 15:44:57 +0100

    perf(26): TypeScript

--- 26/26.ts
+++ 26/26.ts
@@ -3,7 +3,8 @@ function removeDuplicates(nums: number[]): number {
     for (let r = 1; r < nums.length; r++) {
         if (nums[l] !== nums[r]) {
             l++;
-            [nums[l], nums[r]] = [nums[r], nums[l]];
+            nums[l] = nums[r];
+            // [nums[l], nums[r]] = [nums[r], nums[l]];
         }
     }
 
