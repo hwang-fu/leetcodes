818154231701f64cc0171abf7b743ae10d66eb09
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-09 13:02:06 +0100

    solve(13): TypeScript

--- 13/13.ts
+++ 13/13.ts
@@ -1,5 +1,5 @@
 function romanToInt(s: string): number {
-    const map = new Map<string, number>([
+    const tokens = [
         ["M", 1000],
         ["CM", 900],
         ["D", 500],
@@ -12,11 +12,11 @@ function romanToInt(s: string): number {
         ["IX", 9],
         ["V", 5],
         ["IV", 4],
-        ["I", 1],
-    ]);
+        ["I", 1]
+    ];
 
     let ans = 0;
-    for (const [symbol, value] of map) {
+    for (const [symbol, value] of tokens) {
         while (s.startsWith(symbol)) {
             ans += value;
             s = s.slice(symbol.length);
@@ -26,6 +26,33 @@ function romanToInt(s: string): number {
     return ans;
 };
 
+
 const solve1 = romanToInt;
+
+const solve2 = (s: string): number {
+    const map = new Map<string, number>([
+        ["I", 1],
+        ["V", 5],
+        ["X", 10],
+        ["L", 50],
+        ["C", 100],
+        ["D", 500],
+        ["M", 1000],
+    ]);
+
+    let ans = 0;
+    for (let i = 0; i < s.length; i++) {
+        const curr = map.get(s[i])!;
+        const next = map.get(s[i+1]) ?? 0;
+        if (curr < next) {
+            ans += (next - curr);
+            i++;
+        }
+        else {
+            ans += curr;
+        }
+    }
+    return ans;
+};
 
 export {};
