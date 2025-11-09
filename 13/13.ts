2c951f0f6f0857a5407936853424f7c1f5873b0e
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-09 12:50:21 +0100

    solve(13): TypeScript

---
+++ 13/13.ts
@@ -0,0 +1,31 @@
+function romanToInt(s: string): number {
+    const map = new Map<string, number>([
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1],
+    ]);
+
+    let ans = 0;
+    for (const [symbol, value] of map) {
+        while (s.startsWith(symbol)) {
+            ans += value;
+            s = s.slice(symbol.length);
+        }
+    }
+
+    return ans;
+};
+
+const solve1 = romanToInt;
+
+export {};
