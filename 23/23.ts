837082caabe2e0e580fdf46a340788e77ddbd6dc
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-11 19:04:21 +0100

    solve(23): TypeScript

---
+++ 23/23.ts
@@ -0,0 +1,55 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val  = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    if (lists === null || lists.length === 0) {
+        return null;
+    }
+
+    const dummy = new ListNode();
+    let head = dummy;
+
+    const n = lists.length;
+    let stop = false;
+    while (!stop) {
+
+        let idx = 0;
+        let val = Infinity;
+        for (let i = 0; i < n; i++) {
+            const list = lists[i];
+            if (!list) {
+                continue;
+            }
+            if (list.val < val) {
+                val = list.val;
+                idx = i;
+            }
+        }
+
+        if (val !== Infinity) {
+            lists[idx] = lists[idx].next;
+            head.next = new ListNode(val);
+            head = head.next;
+        }
+
+        stop = true;
+        for (const list of lists) {
+            if (list) {
+                stop = false;
+                break;
+            }
+        }
+    }
+
+    return dummy.next;
+};
+
+const mergeKLists1 = mergeKLists;
+
+export {};
