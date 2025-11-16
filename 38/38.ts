a6408a5b1a6fdcf11b2386a621fdbd41bafcaa3a
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-16 20:19:25 +0100

    solve(32): ts iterative

--- 38/38.ts
+++ 38/38.ts
@@ -27,4 +27,36 @@ function countAndSay(n: number): string {
 
 const countAndSayRecursive = countAndSay;
 
+const countAndSayIterative = (n: number): string => {
+    if (n === 1) {
+        return '1';
+    }
+
+    let ans = '1';
+
+    while (n > 1) {
+        const len = ans.length;
+        let temp  = '';
+
+        let count = 0;
+        for (let i = 0; i < len; i++) {
+            count++;
+            if (i < len - 1 && ans[i] === ans[i+1]) {
+                continue;
+            }
+            temp += (count + '' + ans[i]);
+            count = 0;
+        }
+
+        if (count) {
+            temp += (count + '' + ans[len-1]);
+        }
+        ans = temp;
+        n--;
+    }
+
+
+    return ans;
+};
+
 export {};
