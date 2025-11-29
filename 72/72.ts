00a8b10f4fad1dd111a5b10cffc270876e1adcc0
hwang-fu <junzhe.hwangfu@gmail.com>
2025-11-29 21:25:13 +0100

    solve(72): ts

--- 72/72.ts
+++ 72/72.ts
@@ -1,5 +1,35 @@
 function minDistance(word1: string, word2: string): number {
+  const m = word1.length;
+  const n = word2.length;
 
+  const distances: number[][]= new Array(m + 1)
+    .fill(0)
+    .map(_ => new Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    distances[i][0] = i;
+  }
+
+  for (let j = 1; j <= n; j++) {
+    distances[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i-1] === word2[j-1]) {
+        distances[i][j] = distances[i-1][j-1];
+      }
+      else {
+        distances[i][j] = 1 + Math.min(
+          distances[i-1][j],
+          distances[i][j-1],
+          distances[i-1][j-1]
+        );
+      }
+    }
+  }
+
+  return distances[m][n];
 };
 
 const minDistance1 = minDistance;
