1dbe8fb986c052806c7e99380e59513573c0a5e4
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-03 21:25:08 +0100

    perf(36): ts

--- 36/36.ts
+++ 36/36.ts
@@ -51,4 +51,30 @@ function isValidSudoku(board: string[][]): boolean {
   return true;
 };
 
+function isValidSudoku2(board: string[][]): boolean {
+  const rows = Array(9).fill(null).map(() => new Set<string>);
+  const cols = Array(9).fill(null).map(() => new Set<string>);
+  const subgrids = Array(9).fill(null).map(() => new Set<string>);
+
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      const cell = board[r][c];
+      if (cell === '.') {
+        continue;
+      }
+
+      const idx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+      if (rows[r].has(cell) || cols[c].has(cell) || subgrids[idx].has(cell)) {
+        return false;
+      }
+
+      rows[r].add(cell);
+      cols[c].add(cell);
+      subgrids[idx].add(cell);
+    }
+  }
+
+  return true;
+}
+
 export {};
