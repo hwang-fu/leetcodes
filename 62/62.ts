ab50eb67286d6fd5c10d14569d4b0fc9ba55d6da
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-04 19:46:12 +0100

    perf(62): ts

--- 62/62.ts
+++ 62/62.ts
@@ -20,6 +20,38 @@ function uniquePaths(m: number, n: number): number {
   return backtrack(0, 0);
 };
 
+function uniquePaths2(m: number, n: number): number {
+  const memo = new Map<string, number>();
+
+  const backtrack = (downs: number, rights: number): number => {
+    if (downs === m - 1 && rights === n - 1) {
+      return 1;
+    }
+
+    const key = `${downs},${rights}`;
+    if (memo.has(key)) {
+      return memo.get(key)!;
+    }
+
+    let paths = 0;
+
+    if (downs === m - 1) {
+      paths = backtrack(downs, rights + 1);
+    }
+    else if (rights === n - 1) {
+      paths = backtrack(downs + 1, rights);
+    }
+    else {
+      paths = backtrack(downs + 1, rights) + backtrack(downs, rights + 1);
+    }
+
+    memo.set(key, paths);
+    return paths;
+  };
+
+  return backtrack(0, 0);
+};
+
 
 
 export {};
