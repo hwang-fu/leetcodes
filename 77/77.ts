a0b5717a67727b64916d09f5144a35c1a8046ff4
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-05 08:17:18 +0100

    perf(77): ts

--- 77/77.ts
+++ 77/77.ts
@@ -19,4 +19,29 @@ function combine(n: number, k: number): number[][] {
   return ans;
 };
 
+function combine2(n: number, k: number): number[][] {
+  const ans: number[][] = [];
+
+  const indices: number[] = Array.from({ length: k}, (_, i) => i + 1);
+
+  while (true) {
+    ans.push(indices.slice());
+
+    let pos = k - 1;
+    while (pos >= 0 && indices[pos] === n - k + pos + 1) {
+      pos--;
+    }
+
+    if (pos < 0) break;
+
+    indices[pos]++;
+
+    for (let i = pos + 1; i < k; i++) {
+      indices[i] = indices[i - 1] + 1;
+    }
+  }
+
+  return ans;
+};
+
 export {};
