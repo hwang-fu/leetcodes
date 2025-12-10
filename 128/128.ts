ae6d7cc12faa8d34e2a29a236c0c1489131e9c25
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-10 19:25:23 +0100

    solve(128): ts

---
+++ 128/128.ts
@@ -0,0 +1,18 @@
+function longestConsecutive(nums: number[]): number {
+  const set = new Set<number>(nums);
+  let best = 0;
+
+  for (const num of nums) {
+    if (!set.has(num - 1)) {
+      let len = 1;
+      while (set.has(num + len)) {
+        len++;
+      }
+      best = Math.max(best, len);
+    }
+  }
+
+  return best;
+};
+
+export {};
