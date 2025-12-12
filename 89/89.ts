6f2b46a0b361f443cfc9738730782fa989590276
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-12 18:46:09 +0100

    solve(89): ts

---
+++ 89/89.ts
@@ -0,0 +1,34 @@
+function grayCode(n: number): number[] {
+  const ans: number[] = [];
+  const visited = new Set<number>();
+
+  const backtrack = (start: number): boolean => {
+    if (ans.length === 1 << n) {
+      return true;
+    }
+
+    for (let i = 0; i < n; i++) {
+      const next = start ^ (1 << i);
+      if (!visited.has(next)) {
+        visited.add(next);
+        ans.push(next);
+        if (backtrack(next)) {
+          return true;
+        }
+        visited.delete(next);
+        ans.pop();
+      }
+    }
+
+    return false;
+  };
+
+  // start from zero.
+  ans.push(0);
+  visited.add(0);
+  backtrack(0);
+
+  return ans;
+};
+
+export {};
