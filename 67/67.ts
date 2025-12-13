7d907c6a09a96bef5ad609005d4055bc6fdc8535
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-13 16:39:30 +0100

    refactor(67): ts

--- 67/67.ts
+++ 67/67.ts
@@ -34,4 +34,26 @@ function addBinary(a: string, b: string): string {
   return ans;
 };
 
+function addBinary2(a: string, b: string): string {
+  let ans = '';
+  let carry = 0;
+
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry > 0) {
+    const x = i >= 0 ? Number(a[i]) : 0;
+    const y = j >= 0 ? Number(b[j]) : 0;
+    const sum = x + y + carry;
+
+    ans = (sum % 2) + ans;
+    carry = Math.floor(sum / 2);
+
+    i--;
+    j--;
+  }
+
+  return ans;
+}
+
 export {};
