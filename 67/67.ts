0815df40626d5f22feeb8a91a865d97f7666ec2f
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-13 16:37:15 +0100

    solve(67): ts

---
+++ 67/67.ts
@@ -0,0 +1,37 @@
+function addBinary(a: string, b: string): string {
+  let ans = '';
+
+  let carry = 0;
+
+  const xs = a.split('');
+  const ys = b.split('');
+  while (xs.length !== 0 && ys.length !== 0) {
+    const x = xs.pop()!.charCodeAt(0) - '0'.charCodeAt(0);
+    const y = ys.pop()!.charCodeAt(0) - '0'.charCodeAt(0);
+    const sum = x + y + carry;
+    ans = (sum % 2) + ans;
+    carry = Math.floor(sum / 2);
+  }
+
+  while (xs.length !== 0) {
+    const x = xs.pop()!.charCodeAt(0) - '0'.charCodeAt(0);
+    const sum = x + carry;
+    ans = (sum % 2) + ans;
+    carry = Math.floor(sum / 2);
+  }
+
+  while (ys.length !== 0) {
+    const y = ys.pop()!.charCodeAt(0) - '0'.charCodeAt(0);
+    const sum = y + carry;
+    ans = (sum % 2) + ans;
+    carry = Math.floor(sum / 2);
+  }
+
+  if (carry) {
+    ans = '1' + ans;
+  }
+
+  return ans;
+};
+
+export {};
