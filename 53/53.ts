06b02dc0a65458700b4a7268a172a244015fc7d7
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-14 20:47:49 +0100

    solve(53): ts

---
+++ 53/53.ts
@@ -0,0 +1,22 @@
+function maxSubArray(nums: number[]): number {
+  // Constraints:
+  // 1 <= nums.length <= 105
+  // -104 <= nums[i] <= 104
+
+  const n = nums.length;
+  if (n === 1) {
+    return nums[0];
+  }
+
+  let best = -Infinity;
+  for (let windowSize = 1; windowSize <= n; windowSize++) {
+    for (let start = 0; start <= n - windowSize; start++) {
+      let value = nums.slice(start, start + windowSize).reduce((acc, num) => acc + num, 0);
+      best = Math.max(best, value);
+    }
+  }
+
+  return best;
+};
+
+export {};
