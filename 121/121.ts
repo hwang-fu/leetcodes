e0426d11644f12794d556c059b93dc093e5b79d3
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-15 16:36:58 +0100

    solve(121): ts

---
+++ 121/121.ts
@@ -0,0 +1,25 @@
+function maxProfit(prices: number[]): number {
+  let best = 0;
+  let lowest = Infinity;
+  let highest = -Infinity;
+
+  for (let i = 0; i < prices.length; i++) {
+    const price = prices[i];
+
+    if (price < lowest && i < prices.length - 1) {
+      lowest = price;
+      highest = prices[i + 1];
+      best = Math.max(best, highest - lowest);
+      continue;
+    }
+
+    if (price > highest) {
+      highest = price;
+      best = Math.max(best, highest - lowest);
+    }
+  }
+
+  return best;
+};
+
+export {};
