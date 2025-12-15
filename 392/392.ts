b98e25f8e8786a4cbc9bcb833b270efff82abb77
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-15 17:41:39 +0100

    perf(392): ts

--- 392/392.ts
+++ 392/392.ts
@@ -7,4 +7,12 @@ function isSubsequence(s: string, t: string): boolean {
   return xs.length === 0;
 };
 
+function isSubsequence2(s: string, t: string): boolean {
+  let i = 0;
+  for (let j = 0; j < t.length; j++) {
+    if (s[i] === t[j]) i++;
+  }
+  return i === s.length;
+};
+
 export {};
