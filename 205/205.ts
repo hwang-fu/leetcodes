e07e3245f87b952f73ec7777132229c1c201dbcb
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-16 19:55:29 +0100

    refactor(205): ts

--- 205/205.ts
+++ 205/205.ts
@@ -25,4 +25,28 @@ function isIsomorphic(s: string, t: string): boolean {
   return true;
 };
 
+function isIsomorphic2(s: string, t: string): boolean {
+  const n = s.length;
+  const m = t.length;
+  if (n !== m) {
+    return false;
+  }
+
+  const s2t = new Map<string, string>();
+  const t2s = new Map<string, string>();
+
+  for (let i = 0; i < n; i++) {
+    const s_i = s[i];
+    const t_i = t[i];
+
+    if (s2t.has(s_i) && s2t.get(s_i) !== t_i) return false;
+    if (t2s.has(t_i) && t2s.get(t_i) !== s_i) return false;
+
+    s2t.set(s_i, t_i);
+    t2s.set(t_i, s_i);
+  }
+
+  return true;
+};
+
 export {};
