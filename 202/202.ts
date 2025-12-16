3c5ec5a780676dcadf6842d4699241ea65b5edbf
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-16 20:54:14 +0100

    solve(202): ts

---
+++ 202/202.ts
@@ -0,0 +1,22 @@
+function sumOfSquares(n: number): number {
+  let sum = 0;
+  while (n > 0) {
+    const digit = n % 10;
+    sum += digit * digit;
+    n = Math.floor(n / 10);
+  }
+  return sum;
+};
+
+function isHappy(n: number): boolean {
+  const seen = new Set<number>();
+
+  while (n !== 1 && !seen.has(n)) {
+    seen.add(n);
+    n = sumOfSquares(n);
+  }
+
+  return n === 1;
+};
+
+export {};
