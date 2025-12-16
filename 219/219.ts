547fed4f8834e74a0c2c9b53b831fa638e88f464
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-16 21:22:32 +0100

    refactor(219): ts

--- 219/219.ts
+++ 219/219.ts
@@ -11,4 +11,21 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
   return false;
 };
 
+function containsNearbyDuplicate2(nums: number[], k: number): boolean {
+  const window = new Set<number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (window.has(nums[i])) {
+      return true;
+    }
+
+    window.add(nums[i]);
+    if (window.size > k) {
+      window.delete(nums[i - k]);
+    }
+  }
+
+  return false;
+};
+
 export {};
