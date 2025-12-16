dda9bc9291232905896a37d94f79fedb27d31ce7
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-16 20:25:52 +0100

    refactor(160): ts

--- 160/160.ts
+++ 160/160.ts
@@ -23,4 +23,24 @@ function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): Li
   return ptrA;
 };
 
+function getIntersectionNode2(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+  if (headA === null || headB === null) {
+    return null;
+  }
+
+  const set = new Set<ListNode>();
+
+  while (headA) {
+    set.add(headA);
+    headA = headA.next;
+  }
+
+  while (headB) {
+    if (set.has(headB)) return headB;
+    headB = headB.next;
+  }
+
+  return null;
+};
+
 export {};
