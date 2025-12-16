d35c99624ff1b8bfae3b07671cc89ce4673d9797
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-16 19:36:07 +0100

    perf(268): ts

--- 268/268.ts
+++ 268/268.ts
@@ -7,16 +7,26 @@ function missingNumber(nums: number[]): number {
     }
   }
 
-  throw Error('unreachable');
+  throw new Error('unreachable');
 };
 
 function missingNumber2(nums: number[]): number {
   const n = nums.length;
   let xor = nums.length;
   for (let i = 0; i < n; i++) {
+    // a ^ a = 0
+    // a ^ 0 = a
+    // Every number that appears twice cancels out, leaving only the missing one.
     xor ^= (i ^ nums[i]);
   }
   return xor;
+};
+
+function missingNumber3(nums: number[]): number {
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((a, b) => a + b, 0);
+  return expectedSum - actualSum;
 };
 
 export {};
