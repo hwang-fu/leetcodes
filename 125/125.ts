d01dbaeb269267c800fc7d173dce2791ea830cc9
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-17 12:38:55 +0100

    solve(125): ts

--- 125/125.ts
+++ 125/125.ts
@@ -21,4 +21,36 @@ function isPalindrome(s: string): boolean {
   return true;
 };
 
+function isPalindrome2(s: string): boolean {
+  const a = 'a'.charCodeAt(0);
+  const z = 'z'.charCodeAt(0);
+  const zero = '0'.charCodeAt(0);
+  const nine = '9'.charCodeAt(0);
+
+  const isValid = (idx : number) => {
+    const c = s.toLowerCase().charCodeAt(idx);
+    return (a <= c && c <= z) || (zero <= c && c <= nine);
+  };
+
+  const compare = (l : number, r : number) => {
+    return s[l].toLowerCase() === s[r].toLowerCase();
+  };
+
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    while (l <= s.length -1 && !isValid(l)) l++;
+    while (r >= 0 && !isValid(r)) r--;
+    if (l >= r) break;
+    if (!compare(l, r)) {
+      return false;
+    } else {
+      l++;
+      r--;
+    }
+  }
+
+  return true;
+};
+
 export {};
